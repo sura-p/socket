d3412f0b6627950e94f64f29f05f31348be5ec5c
Suraj-Prajapati-Vinove <suraj.prajapati@mail.vinove.com>
2024-10-23 18:35:46 +0530

    updated the backend

--- model/message.js
+++ model/message.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  message: { type: String, required: true },
+  message: { type: String, default:""},
   createdAt: { type: Date, default: Date.now },
 });
 
